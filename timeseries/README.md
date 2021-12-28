59db66a5a505916d79c73243f70aeacbbda481ea
John Yu <byu9@ncsu.edu>
2021-12-28 00:25:00 -0500

    refactor timeseries

---
+++ timeseries/README.md
@@ -0,0 +1,4 @@
+# Timeseries related parameters
+
+1. Specify the scheduling horizon in [Scheduling Horizon](scheduling_horizon.py)
+
---
+++ timeseries/scheduling_horizon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#----------------------------------------------------------------------
+# Specify scheduling horizon in the following format
+# [t1, t2, ...]
+#
+# Make sure the time points are unique and hashable, and that the list
+# is incremental.
+#----------------------------------------------------------------------
+scheduling_horizon = [
+    '2022-01-01T00:00:00Z',
+    '2022-01-01T00:01:00Z',
+]
+
