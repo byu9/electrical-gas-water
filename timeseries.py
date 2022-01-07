a080d4e79b1b70fae134a743a5deaa686362f4ab
John Yu <byu9@ncsu.edu>
2022-01-07 18:50:33 -0500

    add extended_scheduling_horizon to timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -5,7 +5,8 @@
 # [t1, t2, ...]
 #
 # Make sure the time points are unique and hashable, and that the list
-# is incremental.
+# is incremental. The scheduling horizon must not contain 'None' as an
+# element because it is used to create the extended scheduling horizon
 #
 # Discussion with Xiaochu about unit of measure:
 #       Period in the scheduling horizon must be 1 hour.
@@ -14,3 +15,8 @@ scheduling_horizon = [
     '2022-01-01T00:00:00Z',
     '2022-01-01T00:01:00Z',
 ]
+
+# Prepends timepoint 'None'. This is useful for indexing the initial state (a
+# constant) the same way as decision variables for the scheduling horizon.
+extended_initial_timepoint = None
+extended_scheduling_horizon = [extended_initial_timepoint] + scheduling_horizon
