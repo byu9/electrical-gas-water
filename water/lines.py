735be721c07d7237543a82c106efef25564bcd5d
John Yu <byu9@ncsu.edu>
2022-01-27 19:53:16 -0500

    test: increase water line flow lims

--- water/lines.py
+++ water/lines.py
@@ -11,17 +11,17 @@ from math import pi
 flow_lims = {
     # (s, r): (gal_per_h_lo, gal_per_h_hi)
 
-    ('2', '3'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('3', '7'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('3', '4'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('4', '6'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('6', '7'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('4', '5'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('5', '6'): (gpm_to_gph(-600), gpm_to_gph(600)),
-    ('7', '8'): (gpm_to_gph(-600), gpm_to_gph(600)),
+    ('2', '3'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('3', '7'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('3', '4'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('4', '6'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('6', '7'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('4', '5'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('5', '6'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
+    ('7', '8'): (gpm_to_gph(-6000), gpm_to_gph(6000)),
 
     # pump-enabled pipelines
-    ('1', '2'): (gpm_to_gph(0),    gpm_to_gph(600)),
+    ('1', '2'): (gpm_to_gph(0),    gpm_to_gph(6000)),
 }
 
 flow_lims_lo = dict()
